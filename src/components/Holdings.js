a0a8e9b3cd76d302fd4b454dcbfb15df971fb9c6
Ritik Thakur <rk7495933@gmail.com>
2025-07-08 16:44:13 +0530

    install react-router-dom, implement and create  basic components structure and add style for it

---
+++ src/components/Holdings.js
@@ -0,0 +1,11 @@
+function Holdings() {
+  return ( 
+    <div className="container">
+      <div className="row">
+        <h1>Holdings</h1>
+      </div>
+    </div>
+   );
+}
+
+export default Holdings;
---
+++ src/components/WatchList.js
@@ -0,0 +1,11 @@
+function WatchList() {
+  return ( 
+    <div className="container">
+      <div className="row">
+          <h1>Hello</h1>
+      </div>
+    </div>
+   );
+}
+
+export default WatchList;
